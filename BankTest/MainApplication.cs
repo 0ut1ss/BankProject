7f7eeb6d414822b5baf7e96f8890e3b319fccac6
0ut1ss <33423069+0ut1ss@users.noreply.github.com>
2017-12-11 21:50:37 +0200

    Accounts can no longer desposit more than their balance, admin can't withdraw more than normal user's balance

--- BankTest/MainApplication.cs
+++ BankTest/MainApplication.cs
@@ -9,18 +9,9 @@ namespace Bank
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello, is it me you're looking for?");
             Menu m = new Menu();
             m.DisplayMenu();
 
-            //using (BankTestContext btx = new BankTestContext())
-            //{
-            //    string suser = "user1";
-            //    var user = btx.Users.SingleOrDefault(r => r.username == suser);
-            //    var account = btx.Accounts.SingleOrDefault(i => i.id == user.id);
-            //    Console.WriteLine(account.Amount);
-            //    Console.ReadKey();
-            //}
         }
     }
 }
